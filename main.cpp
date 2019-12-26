bf536b2baadaf969ea40af22bd6acdfaa859495e
stephanedurette <stephanedurette@rogers.com>
2019-12-25 16:23:23 -0800

    f

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@
 #include "puzzleLoader.h"
 
 void main() {
-	/*int startingPuzzle[N][N] = {
-	{0, 0, 0, 2, 6, 0, 7, 0, 1},
-	{6, 8, 0, 0, 7, 0, 0, 9, 0},
-	{1, 9, 0, 0, 0, 4, 5, 0, 0},
-	{8, 2, 0, 1, 0, 0, 0, 4, 0},
-	{0, 0, 4, 6, 0, 2, 9, 0, 0},
-	{0, 5, 0, 0, 0, 3, 0, 2, 8},
-	{0, 0, 9, 3, 0, 0, 0, 7, 4},
-	{0, 4, 0, 0, 5, 0, 0, 3, 6},
-	{7, 0, 3, 0, 1, 8, 0, 0, 0}
+	int f[N][N] = {
+	{0, 2, 6, 0, 0, 7, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 3, 6},
+	{0, 0, 0, 6, 9, 8, 0, 0, 7},
+	{0, 0, 0, 0, 5, 0, 2, 0, 8},
+	{0, 0, 7, 9, 0, 2, 1, 0, 0},
+	{4, 0, 2, 0, 8, 0, 0, 0, 0},
+	{2, 0, 0, 1, 4, 9, 0, 0, 0},
+	{8, 4, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 8, 0, 0, 4, 5, 0}
 	};
-	init_puzzle(startingPuzzle);
-	print_puzzle();
-	recursive_solve_puzzle();
-	print_puzzle();*/
-	int f[N][N];
-	loadPuzzle(f, 51);
+	//init_puzzle(startingPuzzle);
+	//print_puzzle();
+	//recursive_solve_puzzle();
+	//print_puzzle();*/
+	//int f[N][N];
+	//loadPuzzle(f, 1);
 	init_puzzle(f);
 	print_puzzle();
 	recursive_solve_puzzle();
